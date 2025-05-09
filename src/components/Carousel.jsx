737a1f6d1285d3e3f2755476aa9b2fcab312e403
44120N <106742333+44120N@users.noreply.github.com>
2025-05-09 08:18:57 +0700

    Update Carousel.jsx

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -40,7 +40,7 @@ const Carousel = ({ slides, time }) => {
             sx={{
                 position: "relative",
                 width: "100%",
-                height: { xs: "60vh", sm: "80vh" },
+                height: " auto",
                 overflow: "hidden",
             }}
         >
